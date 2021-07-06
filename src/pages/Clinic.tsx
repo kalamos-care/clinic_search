1eacc0ffb61a9cff4abc934eb06b646a1d13292e
Brad <bbthorson@gmail.com>
2021-07-06 15:36:28 -0400

    added underwater clinic info display

--- src/pages/Clinic.tsx
+++ src/pages/Clinic.tsx
@@ -1,17 +1,23 @@
 import React, { FC } from 'react';
-
 import { useParams } from 'react-router-dom';
-
 import { makeStyles } from '@material-ui/core/styles';
+import axios, { AxiosResponse } from 'axios';
 
 import { Grid } from '@material-ui/core/';
 
 import PageTitle from '../components/PageTitle';
 
+
 const useStyles = makeStyles((theme) => ({
 
 }));
 
+const instance = axios.create({
+    baseURL: 'https://underwater.hack.fail/v1',
+    timeout: 15000,
+});
+
+const responseBody = (response: AxiosResponse) => response.data;
 interface RouteParams {
     id: string
 }
@@ -19,11 +25,17 @@ interface RouteParams {
 export const Clinic: FC = () => {
     const classes = useStyles();
 
-    const params = useParams<RouteParams>();
+    const param = useParams<RouteParams>();
+
+    const getClinicData = instance.get(`/test-centers/${param.id}`).then(responseBody);
+    console.log(getClinicData);
 
     return (
         <Grid container>
-            <PageTitle title={`This is clinic ${params.id}`} />
+            <PageTitle title={`This is clinic ${param.id}`} />
+            <Grid item xs={12}>
+                <pre>{JSON.stringify(getClinicData, null, 2)}</pre>
+            </Grid>
         </Grid>
     );
 };
