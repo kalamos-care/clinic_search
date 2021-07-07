65fe52018850fe5b1e22f23698e307d7c6aaecb2
Brad <bbthorson@gmail.com>
2021-07-07 17:48:01 -0400

    moved theme to it's own .ts file

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,16 @@
 import React, { FC } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+//import { BrowserRouter as Router } from 'react-router-dom';
 import { HashRouter } from 'react-router-dom';
 
 import { ThemeProvider } from '@material-ui/styles';
-import { createMuiTheme, makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import { PublicRoutes } from './PublicRoutes';
 import ScrollToTop from './ScrollToTop';
 
-import GTHaptikMediumWoff2 from './assets/fonts/GT-Haptik-Medium-2.woff2';
-import "@fontsource/arimo";
+import theme from './theme';
 
-
-const gthaptikmedium = {
-  fontFamily: 'GT-Haptik-Medium',
-  fontStyle: 'normal',
-  fontWeight: 400,
-  src: `
-    local('GT-Haptik-Medium'),
-    url(${GTHaptikMediumWoff2}) format('woff2')
-  `,
-};
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: '#67568c',
-      main: '#463366',
-      dark: '#301e4e',
-    },
-    secondary: {
-      main: '#ff6e6c',
-    },
-    text: {
-      primary: "#1f1235",
-      secondary: "#1b1425",
-    }
-  },
-  typography: {
-    button: {
-      fontFamily: "GT-Haptik-Medium",
-    },
-    h1: {
-      fontFamily: "GT-Haptik-Medium",
-    },
-    h2: {
-      fontFamily: "GT-Haptik-Medium",
-    },
-    h3: {
-      fontFamily: "GT-Haptik-Medium",
-    },
-    h4: {
-      fontFamily: "GT-Haptik-Medium",
-    },
-    h5: {
-      fontFamily: "GT-Haptik-Medium",
-    },
-    h6: {
-      fontFamily: "GT-Haptik-Medium",
-    },
-    fontFamily: [
-      'Arimo',
-      'sans-serif',
-    ].join(','),
-  },
-  overrides: {
-    MuiCssBaseline: {
-      '@global': {
-        '@font-face': [gthaptikmedium],
-      },
-    },
-  },
-});
 
 const useStyles = makeStyles((theme) => ({
 
