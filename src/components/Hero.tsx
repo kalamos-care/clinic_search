f37ad2db2b8ffd632cde88f7f6971043aa1a2172
Brad <bbthorson@gmail.com>
2021-07-08 18:38:58 -0400

    shrunk header font size

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,10 +16,10 @@ export const Hero: FC = () => {
     return (
         <Grid container spacing={2} justify="center" className={classes.hero}>
             <Grid item xs={12}>
-                <Typography variant="h3" gutterBottom>PrEP is lifesaving—but what is PrEP?</Typography>
-                <Typography variant="h5" gutterBottom>Often times, we think of PrEP as a pill—and it is!</Typography>
-                <Typography variant="h5" gutterBottom>It's more than that though. PreExposure Prophylaxis means taking actions to protect yourself and your community before having sex and other activities.</Typography>
-                <Typography variant="h6" gutterBottom>If you're interested in PrEP medication or other sexual health resources, just enter your zip code below.</Typography>
+                <Typography variant="h3" gutterBottom>PrEP is lifesaving–but what is PrEP?</Typography>
+                <Typography variant="h6" gutterBottom>Often times, we think of PrEP as a pill, and it is!</Typography>
+                <Typography variant="h6" gutterBottom>It's more than that though. PreExposure Prophylaxis means taking actions to protect yourself and your community before having sex and other activities.</Typography>
+                <Typography variant="subtitle1" gutterBottom>If you're interested in PrEP medication or other sexual health resources, just enter your zip code below.</Typography>
             </Grid>
         </Grid>
     );
