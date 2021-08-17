38c65596f8aabf253e9c6000fb163e7944bc0e9e
Brad <bbthorson@gmail.com>
2021-08-16 21:33:37 -0400

    added styling classes to SearchPanel

--- src/components/ClinicSearch.tsx
+++ src/components/ClinicSearch.tsx
@@ -8,89 +8,95 @@ import { Button, TextField, Grid, Typography, List, ListItem, ListItemText, Link
 import { ClinicAPI } from '../api/clinic_api';
 //import { ClinicType } from '../models/clinic.interface';
 
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles((theme) => ({
+  clinicsList: {
+
+  },
+}));
+
 // ClinicSearch should handle the request for data and then pass that to ClinicList. ClinicList currently has that functionality.
-// Got useEffect code from https://reactjs.org/docs/faq-ajax.html because Axios wasn't working
 
 const ClinicSearch: FC = () => {
-    const searchTermEl = useRef<HTMLInputElement>(null);
+  const classes = useStyles();
 
-    //const [ClinicListData, setClinicListData] = React.useState<ClinicType[]>();
-    const [ClinicListData, setClinicListData] = React.useState<any>();
-    useEffect(() => {
-      if (searchTermEl.current != null) {
-        ClinicAPI.getClinicsByZip(searchTermEl.current.value)
-          .then(data => setClinicListData(data))
-      }
-      ClinicAPI.getBaseClinics().then(data => setClinicListData(data))
-    }, []);
-  
+  const searchTermEl = useRef<HTMLInputElement>(null);
 
-    return (
-        <Grid container>
-        <Grid item xs={12}>
-          <Typography variant="h4" component="h2">
-            Find a Clinic
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <form>
-            <TextField
-              id="searchTerm"
-              type="text"
-              inputRef={searchTermEl}
-              placeholder="Enter Zip Code"
-            />
-            <Button
-              variant="text"
-              color="default"
-              type="button"
-              onClick={() => {
-                if (searchTermEl.current != null)
-                  ClinicAPI
-                    .getClinicsByZip(searchTermEl.current.value)
-                    .then(data => setClinicListData(data))
-              }}
-            >
-              Submit
-            </Button>
-          </form>
-        </Grid>
-        {/* <ClinicList zip={searchTermEl.current.value} /> */}
-        <Grid item xs={12}>
-          {ClinicListData ?
-            <List className="">
-              {ClinicListData.data.map((clinic: any) =>
-                <ListItem alignItems="flex-start" key={clinic.id}>
-                  <Link component={RouterLink} to={`/clinic/${clinic.id}`}>
-                    <ListItemText
-                      primary={clinic.title}
-                      secondary={
-                        <React.Fragment>
-                          <Typography
-                            component="span"
-                            variant="body2"
-                            className=""
-                            color="textPrimary"
-                          >
-                            {`${clinic.location.city}, ${clinic.location.state} ${clinic.location.zipcode}`}
-                          </Typography>
-                          {[
-                            clinic.email,
-                          ]}
-                        </React.Fragment>
-                      }
-                    />
-                  </Link>
-                  <Divider />
-                </ListItem>
-              )}
-            </List>
-            :
-            <p>No data</p>
-          }
-        </Grid>
+  //const [ClinicListData, setClinicListData] = React.useState<ClinicType[]>();
+  const [ClinicListData, setClinicListData] = React.useState<any>();
+  useEffect(() => {
+    if (searchTermEl.current != null) {
+      ClinicAPI.getClinicsByZip(searchTermEl.current.value)
+        .then(data => setClinicListData(data))
+    }
+    ClinicAPI.getBaseClinics().then(data => setClinicListData(data))
+  }, []);
+
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <Typography variant="h4" component="h2" gutterBottom>
+          Find a Clinic
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          Our clinic database is built on the incredible work of the CDC and their NPIN project.
+        </Typography>
       </Grid>
-    );
+      <Grid item xs={12}>
+        <form>
+          <TextField
+            id="searchTerm"
+            type="text"
+            inputRef={searchTermEl}
+            placeholder="Enter Zip Code"
+          />
+          <Button
+            variant="text"
+            color="default"
+            type="button"
+            onClick={() => {
+              if (searchTermEl.current != null)
+                ClinicAPI
+                  .getClinicsByZip(searchTermEl.current.value)
+                  .then(data => setClinicListData(data))
+            }}
+          >
+            Search
+          </Button>
+        </form>
+      </Grid>
+      {/* <ClinicList zip={searchTermEl.current.value} /> */}
+      <Grid item xs={12}>
+        {ClinicListData ?
+          <List className="clinicsList">
+            {ClinicListData.data.map((clinic: any) =>
+              <ListItem alignItems="flex-start" key={clinic.id}>
+                <Link component={RouterLink} to={`/clinic/${clinic.id}`}>
+                  <ListItemText
+                    primary={clinic.title}
+                    secondary={
+                      <React.Fragment>
+                        <Typography
+                          component="span"
+                          variant="body2"
+                          className=""
+                          color="textPrimary"
+                        >
+                          {`${clinic.location.city}, ${clinic.location.state} ${clinic.location.zipcode}`}
+                        </Typography>
+                      </React.Fragment>
+                    }
+                  />
+                </Link>
+              </ListItem>
+            )}
+          </List>
+          :
+          <p>No data</p>
+        }
+      </Grid>
+    </Grid>
+  );
 };
 
 export default ClinicSearch;
